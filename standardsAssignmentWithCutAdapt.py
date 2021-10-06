c01aa70355787fcfb052ce2cd0bd0e7a6d02126a
MViscardi-UCSC <mviscard@ucsc.edu>
2021-10-06 13:06:46 -0700

    removed pandas import

--- standardsAssignmentWithCutAdapt.py
+++ standardsAssignmentWithCutAdapt.py
@@ -17,7 +17,6 @@ Going to try and pull the assigned adapters with this:
     https://cutadapt.readthedocs.io/en/stable/guide.html#info-file
 """
 import subprocess
-import pandas as pd
 
 
 def cutadapt_call(adaptor_file, output_file, input_file):
@@ -37,6 +36,6 @@ def cutadapt_call(adaptor_file, output_file, input_file):
 if __name__ == '__main__':
     path_to_fa = "/data16/marcus/working/210706_NanoporeRun_riboD-and-yeastCarrier_0639_L3/" \
                    "output_dir/cat_files/cat.fastq"
-    cutadapt_call(adaptor_file="/data16/marcus/scripts/nanoporePipelineScripts/standardUMIs.fa",
+    cutadapt_call(adaptor_file="/data16/marcus/scripts/nanoporePipelineScripts/standardUMIs_plusUpstreamSeq.fa",
                   output_file="./deleteme.fa",
                   input_file=path_to_fa)
