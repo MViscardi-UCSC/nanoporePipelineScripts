31a4a1e9050830596ba9f4fc8f1db757e5050ee7
MViscardi-UCSC <mviscard@ucsc.edu>
2021-11-09 17:05:44 -0800

    Nice scatter plot build up

--- comparingPolyATails.py
+++ comparingPolyATails.py
@@ -52,7 +52,7 @@ def count_nts_mapped(df_from_sam: pd.DataFrame):
     return nt_sum, mean, median, min, max, df_from_sam.shape[0]
 
 
-if __name__ == '__main__':
+def old_main():
     path_to_roach = "/data16/marcus/prefix/210106_nanopolish_wRoachData/output_dir_youngadultrep1tech1/cat_allReads.sam"
     path_to_my = "/data16/marcus/prefix/210528_NanoporeRun_0639_L3s/output_dir/cat_files/cat.sorted.sam"
     dictionary = {"Roach data (young adult worms - replicate 1)": path_to_roach,
@@ -70,3 +70,58 @@ if __name__ == '__main__':
           f"Median:\t{returns[0][2]:10.0f}\t{returns[1][2]:10.0f}\n"
           f"Min:  \t{returns[0][3]:10.0f}\t{returns[1][3]:10.0f}\n"
           f"Max:  \t{returns[0][4]:10.0f}\t{returns[1][4]:10.0f}")
+
+
+def seaborn_tail_length_scatter(lib_list, min_hits, plot_column="mean_polya_length"):
+    import seaborn as sea
+    import matplotlib.pyplot as plt
+    
+    if len(lib_list) != 2:
+        raise NotImplementedError(f"Please only provide 2 libraries, you passed: {lib_list}")
+    
+    reads_df, compressed_df = load_and_merge_lib_parquets(lib_list)
+    x_lib, y_lib = lib_list
+
+    min_hit_df = compressed_df[compressed_df['gene_hits'] >= min_hits]
+    x_axis_df = min_hit_df[min_hit_df.lib == x_lib][["gene_id",
+                                                             "gene_name",
+                                                             "gene_hits",
+                                                             "mean_polya_length"]]
+    y_axis_df = min_hit_df[min_hit_df.lib == y_lib][["gene_id",
+                                                             "gene_name",
+                                                             "gene_hits",
+                                                             "mean_polya_length"]]
+
+    plot_df = pd.merge(x_axis_df, y_axis_df, on=["gene_id", "gene_name"],
+                       suffixes=(f"_{x_lib}",
+                                 f"_{y_lib}"))
+    max_mean = min_hit_df[plot_column].max()
+    max_mean += 10
+    min_mean = min_hit_df[plot_column].min()
+    min_mean -= 10
+    
+    sea.set_style("whitegrid")
+    sea.set_context("notebook")
+    plt.style.context("seaborn-whitegrid")
+    plot_size = 5
+    fig, ax = plt.subplots(figsize=(plot_size, plot_size))
+    
+    fig = sea.scatterplot(data=plot_df, ax=ax,
+                          x=f"{plot_column}_{x_lib}",
+                          y=f"{plot_column}_{y_lib}",
+                          color=(0.2, 0.2, 0.2, 0.5))
+    fig.set_xlim(round(min_mean/10)*10, round(max_mean/10)*10)
+    fig.set_xticks(range(round(min_mean/10)*10, round(max_mean/10)*10, 15))
+    fig.set_ylim(round(min_mean/10)*10, round(max_mean/10)*10)
+    fig.set_yticks(range(round(min_mean/10)*10, round(max_mean/10)*10, 15))
+    fig.set_title(f"Mean poly(A) Tail Lengths per Gene called by Nanopolish\n(hits cutoff of {min_hits}reads/gene)")
+    fig.set_xlabel(f"Mean Tail Length Per Gene from: {x_lib} (nts)")
+    fig.set_ylabel(f"Mean Tail Length Per Gene from: {y_lib} (nts)")
+    sea.despine()
+    plt.tight_layout()
+    plt.show()
+    print("done?")
+
+
+if __name__ == '__main__':
+    seaborn_tail_length_scatter(["totalRNA2", "polyA2"], 40)
