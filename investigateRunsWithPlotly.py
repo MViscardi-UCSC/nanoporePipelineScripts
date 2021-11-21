4a7e0b7c0e429aa18b7ac0e2bdd37b6864a6910b
MViscardi-UCSC <mviscard@ucsc.edu>
2021-11-21 12:36:27 -0800

    Placeholder script for now

---
+++ investigateRunsWithPlotly.py
@@ -0,0 +1,19 @@
+"""
+investigateRunsWithPlotly.py
+Marcus Viscardi,    November 16, 2021
+
+Trying to compare things like read_hits to gc_content or otherwise,
+as this will hopefully provide more evidence that MtDNA is weird!
+"""
+import seaborn as sea
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+def plotly_gc_v_hits():
+    pass
+
+
+if __name__ == '__main__':
+    plotly_gc_v_hits()
